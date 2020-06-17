52f3c213a15a56b4f8376ea5bb43df6a05c1a70e
Joshua <meraki.dot@gmail.com>
2020-06-17 16:17:43 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,13 @@ const About = () => (
     <div className="container" id="about">
       <h1 className="title">About</h1>
       <p className="read-text">
-        Thanh is a licensed esthetician, certified permanent cosmetic artist and
-        eyelash guru client feels comfortable and assured. In preparation for
+        Thanh is a certified permanent cosmetic artist and eyelash guru. She
+        opened Lush Eye Bar in Alameda, California in 2018. She understands and
+        happily meets the needs of clients of all ages. She is detail-oriented
+        and meticulous about her craft. This is apparent in every treatment; she
+        makes sure each client feels comfortable and assured. In preparation for
         developing this business, she’s thoroughly studied esthetics and it
-        reawakened a passion for beauty. Which she now applies in helping people
+        reawakened a passion for beauty which she now applies in helping people
         enhance and nurture their appearances.{" "}
       </p>
       <p className="read-text">
@@ -19,13 +22,13 @@ const About = () => (
         she’s always had an eye for style. She has the ability to look at
         something, reimagine it and recreate it. Along with founding Lush Eye
         Bar, she is a licensed esthetician. She is certified by Daria Chupry,
-        Eyes by Sally and Thuy Tien as a permanent cosmetic artist. She is also
-        an eyelash ace certified by Lash Box LA, Co0kies Beauty Bar and Olivia
-        and Lash.
+        Eye’s by Sally, and Thuy Tien as a permanent cosmetic artist. She is
+        also an eyelash ace certified by Lash Box LA, Cookies Beauty Bar, and
+        Olivia and Lash.
       </p>
       <p className="read-text">
-        During her free time, she loves to unwind by traveling and doing DIY
-        projects at home.
+        During her free-time, she loves to unwind by traveling and doing DIY
+        projects at hom
       </p>
     </div>
   </Layout>
