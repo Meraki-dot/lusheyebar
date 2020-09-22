a455a2552057c10d0c639fff97b764e8e0e0dbf4
Joshua Han <52216004+Meraki-dot@users.noreply.github.com>
2020-09-21 23:54:57 -0700

    schedule page changes

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,27 +1,47 @@
 import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header_components/header"
-import Footer from "../components/footer_components/Footer"
 
 const Schedule = () => {
+
   return (
     <div className="schedule-container">
-      <Header />
-      <iframe
-        id="schedulista-widget-00"
-        src="https://www.schedulista.com/schedule/lusheyebar?mode=widget"
-        allowtransparency="true"
-        frameborder="0"
-        scrolling="no"
-        width="100%"
-        height="900px"
-      ></iframe>
-      <script
-        id="schedulista-widget-script-00"
-        type="text/javascript"
-        src="https://www.schedulista.com/schedule/lusheyebar/widget.js"
-      ></script>
-      <Footer />
+      <div className="desktop-schedule-container">
+        <Header />
+        <iframe
+          id="schedulista-widget-00"
+          src="https://www.schedulista.com/schedule/lusheyebar?mode=widget"
+          allowtransparency="true"
+          frameborder="0"
+          scrolling="yes"
+          width="100%"
+          height= "1200px"
+          onload="this.height=this.contentWindow.document.body.scrollHeight;"
+        ></iframe>
+        <script
+          id="schedulista-widget-script-00"
+          type="text/javascript"
+          src="https://www.schedulista.com/schedule/lusheyebar/widget.js"
+        ></script>
+      </div>
+      <div className="mobile-schedule-container">
+        <Header />
+        <iframe
+          id="schedulista-widget-00"
+          src="https://www.schedulista.com/schedule/lusheyebar?mode=widget"
+          allowtransparency="true"
+          frameborder="0"
+          scrolling="no"
+          width="100%"
+          height="2700px"
+          onload="scroll(0,0);"
+        ></iframe>
+        <script
+          id="schedulista-widget-script-00"
+          type="text/javascript"
+          src="https://www.schedulista.com/schedule/lusheyebar/widget.js"
+        ></script>
+      </div>
     </div>
   )
 }
